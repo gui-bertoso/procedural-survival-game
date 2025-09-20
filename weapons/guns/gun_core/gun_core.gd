1c2499e736bbb4992e9380ab0e5401ce382e2c43
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-09-20 15:16:47 -0300

    feat: new resources
    guns bench updates

---
+++ weapons/guns/gun_core/gun_core.gd
@@ -0,0 +1,6 @@
+extends Node3D
+class_name GunCore
+
+@onready var pipe_snap = $ComponentsSnaps/PipeSnap
+@onready var handle_snap = $ComponentsSnaps/HandleSnap
+@onready var butt_snap = $ComponentsSnaps/ButtSnap
