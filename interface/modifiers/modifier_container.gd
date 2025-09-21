459faec184ea510c3eda7e6fa84b0ca8d3bffa7a
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-09-20 22:14:01 -0300

    chore: gun bench

--- interface/modifiers/modifier_container.gd
+++ interface/modifiers/modifier_container.gd
@@ -1,5 +1,24 @@
 extends ColorRect
 
-func set_mod(modification: GunModification):
-	$TextureRect.texture = modification.modification_image
-	$Label.text = modification.modification_name
+var can_click = false
+
+var modification = null
+var node = null
+
+func set_mod(mod: GunModification):
+	$TextureRect.texture = mod.modification_image
+	$Label.text = mod.modification_name.to_snake_case().replace("_", " ").capitalize()
+	modification = mod
+
+func _process(delta: float) -> void:
+	if can_click:
+		if Input.is_action_just_pressed("ui_select"):
+			node.select_modification(modification)
+
+func _on_mouse_entered() -> void:
+	color = Color("878700ff")
+	can_click = true
+
+func _on_mouse_exited() -> void:
+	color = Color("1e1e1e")
+	can_click = false
