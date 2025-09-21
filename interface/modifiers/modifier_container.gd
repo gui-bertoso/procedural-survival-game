173006249eb16d50fb2e88b96d98e28476a229a8
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-09-20 22:57:10 -0300

    develop

--- interface/modifiers/modifier_container.gd
+++ interface/modifiers/modifier_container.gd
@@ -1,24 +1,43 @@
 extends ColorRect
+class_name ModificationRecipe
+
+var item = null
+
+var interface = null
 
 var can_click = false
+var selected = false
 
-var modification = null
-var node = null
-
-func set_mod(mod: GunModification):
-	$TextureRect.texture = mod.modification_image
-	$Label.text = mod.modification_name.to_snake_case().replace("_", " ").capitalize()
-	modification = mod
+func set_modification(_item):
+	item = _item.duplicate(true)
+	$TextureRect.texture = item.modification_image
+	$Label.text = item.modification_name.to_snake_case().replace("_", " ").capitalize()
 
 func _process(delta: float) -> void:
 	if can_click:
 		if Input.is_action_just_pressed("ui_select"):
-			node.select_modification(modification)
+			match selected:
+				true:
+					selected = false
+					interface.deselect_modification()
+					color = Color(0.1, 0.1, 0.1)
+				false:
+					selected = true
+					interface.select_modification(self)
+					color = Color(0.6, 0.6, 0.2)
 
 func _on_mouse_entered() -> void:
-	color = Color("878700ff")
+	print("ENTERED")
+	if selected:
+		color = Color(0.9, 0.9, 0.5)
+	else:
+		color = Color(0.2, 0.2, 0.2)
 	can_click = true
 
 func _on_mouse_exited() -> void:
-	color = Color("1e1e1e")
+	print("EXITED")
+	if selected:
+		color = Color(0.6, 0.6, 0.2)
+	else:
+		color = Color(0.1, 0.1, 0.1)
 	can_click = false
