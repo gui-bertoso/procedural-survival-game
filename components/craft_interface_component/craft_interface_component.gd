c9c5e66c5beb618d59f8b6d4aac181f1829ce3db
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-09-22 23:21:33 -0300

    feat: update scenes and scripts

--- components/craft_interface_component/craft_interface_component.gd
+++ components/craft_interface_component/craft_interface_component.gd
@@ -2,8 +2,15 @@ extends Control
 class_name CraftInterfaceComponent
 
 @onready var _recipes_container: VBoxContainer = $HBoxContainer/ColorRect/ScrollContainer/VBoxContainer
+@onready var _ingredients_container: VBoxContainer = $HBoxContainer/ColorRect2/VBoxContainer/ScrollContainer/VBoxContainer
+@onready var _craft_menu: ColorRect = $HBoxContainer/ColorRect2
+@onready var _craft_button: Button = $HBoxContainer/ColorRect2/VBoxContainer/Button
+@onready var _craft_name_label: Label = $HBoxContainer/ColorRect2/VBoxContainer/Label
+@onready var _craft_type_label: Label = $HBoxContainer/ColorRect2/VBoxContainer/HBoxContainer/Label4
+@onready var _craft_quantity_label: Label = $HBoxContainer/ColorRect2/VBoxContainer/HBoxContainer2/Label4
 
 @onready var craft_recipe_scene: PackedScene = preload("uid://4t4hdhigv82e")
+@onready var craft_ingredient_scene: PackedScene = preload("uid://d3cf6gb4ldt7y")
 
 @export var crafts: Array[Item] = []
 
@@ -13,51 +20,50 @@ func load_recipes() -> void:
 	for i in _recipes_container.get_children():
 		i.queue_free()
 	for i in crafts:
-		var a = load("res://components/craft_recipe.tscn").instantiate()
+		var a = craft_recipe_scene.instantiate()
 		a.set_item(i)
 		a.craft_interface = self
-		$HBoxContainer/ColorRect/ScrollContainer/VBoxContainer.add_child(a)
+		_recipes_container.add_child(a)
 
 func unselect_recipe() -> void:
-	for i in $HBoxContainer/ColorRect2/VBoxContainer/ScrollContainer/VBoxContainer.get_children():
+	for i in _ingredients_container.get_children():
 		i.queue_free()
-	$HBoxContainer/ColorRect2.visible = false
+	_craft_menu.visible = false
 
 func select_recipe(recipe: CraftRecipe) -> void:
-	$HBoxContainer/ColorRect2.visible = true
+	_craft_menu.visible = true
 	if selected_recipe != null and selected_recipe.item != recipe.item:
 		selected_recipe.unselect()
 	selected_recipe = recipe
-	for i in $HBoxContainer/ColorRect2/VBoxContainer/ScrollContainer/VBoxContainer.get_children():
+	for i in _ingredients_container.get_children():
 		i.queue_free()
 	for i in selected_recipe.item.item_craft_recipe:
-		var a = load("res://components/craft_ingredient.tscn").instantiate()
+		var a = craft_ingredient_scene.instantiate()
 		var ie = i.duplicate(true)
 		ie.item_amount = selected_recipe.item.item_craft_recipe[i]
-		print(ie.item_amount)
 		a.set_item(ie)
-		$HBoxContainer/ColorRect2/VBoxContainer/ScrollContainer/VBoxContainer.add_child(a)
-	$HBoxContainer/ColorRect2/VBoxContainer/Label.text = selected_recipe.item.item_name.to_snake_case().replace("_", " ").capitalize()
-	$HBoxContainer/ColorRect2/VBoxContainer/HBoxContainer/Label4.text = selected_recipe.item.item_type.to_upper()
-	$HBoxContainer/ColorRect2/VBoxContainer/HBoxContainer2/Label4.text = str(selected_recipe.item.item_craft_quantity)
+		_ingredients_container.add_child(a)
+	_craft_name_label.text = selected_recipe.item.item_name.to_snake_case().replace("_", " ").capitalize()
+	_craft_type_label.text = selected_recipe.item.item_type.to_upper()
+	_craft_quantity_label.text = str(selected_recipe.item.item_craft_quantity)
 	can_craft()
 
 func can_craft() -> void:
 	var can_craft_ingredients = 0
-	for i in $HBoxContainer/ColorRect2/VBoxContainer/ScrollContainer/VBoxContainer.get_children():
+	for i in _ingredients_container.get_children():
 		if i.can_craft:
 			can_craft_ingredients += 1
 	if can_craft_ingredients >= selected_recipe.item.item_craft_recipe.size():
-		$HBoxContainer/ColorRect2/VBoxContainer/Button.disabled = false
+		_craft_button.disabled = false
 	else:
-		$HBoxContainer/ColorRect2/VBoxContainer/Button.disabled = true
+		_craft_button.disabled = true
 
 func _on_button_button_up() -> void:
 	var new_item: Item = selected_recipe.item.duplicate(true)
 	new_item.item_amount = selected_recipe.item.item_amount
 	if Globals.inventory:
 		Globals.inventory.add_item(new_item)
-	$HBoxContainer/ColorRect2.visible = false
+	_craft_menu.visible = false
 	selected_recipe.unselect()
 	selected_recipe = null
-	$HBoxContainer/ColorRect2/VBoxContainer/Button.disabled = true
+	_craft_button.disabled = true
--- itens/item_template/item.gd
+++ itens/item_template/item.gd
@@ -4,14 +4,14 @@ class_name Item
 @export var item_name = "ItemName"
 @export var item_descryption = "A item"
 @export_enum("resource", "weapon", "equipable") var item_type = ""
-@export var item_image: Texture2D = preload("res://assets/Sprite-0006.png")
+@export var item_image: Texture2D = preload("uid://ri6oq4y3srj1")
 
 @export_category("Subtypes")
 @export_enum("head_armor", "body_armor", "leg_armor", "foot_armor", "head_armor") var equipable_type = ""
 @export_enum("gun", "sword", "knight", "axe", "pickaxe", "shovel", "hammer") var weapon_type = ""
 
 @export_category("3D")
-@export var item_mesh: PackedScene = preload("res://assets/template_item_mesh/template_item_mesh.tscn")
+@export var item_mesh: PackedScene = preload("uid://d4dd4jvhoe24o")
 @export var item_scene: PackedScene = null
 
 @export_category("Others")
